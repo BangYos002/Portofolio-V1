50eb009feba379e0425a8351d4663125455eed18
BangYos002 <abangsidiq93@gmail.com>
2024-09-01 17:50:56 +0700

    add contact

--- main.js
+++ main.js
@@ -7,11 +7,27 @@ function toggleDetail(e) {
         $(target).html("Less Info").addClass("active")
     }
 
-
     const item = $(target).parents(".about-exp-item")
     const detail = $(item).children(".about-exp-item-detail")
-
     $(detail).slideToggle()
-    
+}
 
+function onFormSubmit(e) {
+    e.preventDefault()
+    const email = $("#inp_email")
+    const subject = $("#inp_subject")
+    const message = $("#inp_message")
+
+    if(!$(email).val()) {
+        alert("Email required")
+    } else if (!$(subject).val()) {
+        alert("Subject required")
+    } else if (!$(message).val()) {
+        alert("Message required")
+    } else {
+        alert("Form Submitted")
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
+    }
 }
